2c29ccc2c005f0b50c3ead9bebe842f9cbc287c5
Theocode12 <vchidubem9@gmail.com>
2025-03-15 14:38:25 +0100

    main utils for app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,52 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PollsModule } from './polls/polls.module';
+import { PollOptionsModule } from './poll_options/poll_options.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { Poll } from './polls/entities/poll.entity';
+import { PollOption } from './poll_options/entities/poll_option.entity';
+import { DataSource } from 'typeorm';
+import { AuthModule } from './auth/auth.module';
+import { VotesModule } from './votes/votes.module';
+import { Vote } from './votes/entities/vote.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'poll_system',
+      entities: [User, Poll, PollOption, Vote],
+      synchronize: true,
+    }),
+    UsersModule,
+    PollsModule,
+    PollOptionsModule,
+    AuthModule,
+    VotesModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {
+    // this.testDatabaseConnection();
+  }
+
+  // async testDatabaseConnection() {
+  //   try {
+  //     const result = await this.dataSource.query('SELECT 1+1 AS result');
+  //     console.log('✅ Database connection successful:', result);
+  //   } catch (error) {
+  //     console.error('❌ Database connection failed:', error);
+  //   }
+  // }
+}
